9acbaee29b2ea501a0132a85e4c6c190c1c5c5aa
Mattias Andrée <maandree@operamail.com>
2014-03-01 10:48:19 +0100

    add uname
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/plugins/uname.py
@@ -0,0 +1,52 @@
+# -*- python -*-
+'''
+xpybar – xmobar replacement written in python
+Copyright © 2014  Mattias Andrée (maandree@member.fsf.org)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from util import *
+
+
+class Uname:
+    '''
+    Retrieve certain system information
+    
+    @variable  kernel_name:str         The kernel name
+    @variable  nodename:str            The network node hostname
+    @variable  kernel_release:str      The kernel release
+    @variable  kernel_version:str      The kernel version
+    @variable  machine:str             The machine hardware name
+    @variable  processor:str?          The processor type, `None` if unknown
+    @variable  hardware_platform:str?  The hardware platform, `None` if unknown
+    @variable  operating_system:str    The operating system
+    '''
+    
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        u = lambda s : None if s == 'unknown' else s
+        self.kernel_name       = spawn_read('uname', '-s')
+        self.nodename          = spawn_read('uname', '-n')
+        self.kernel_release    = spawn_read('uname', '-r')
+        self.kernel_version    = spawn_read('uname', '-v')
+        self.machine           = spawn_read('uname', '-m')
+        self.processor         = u(spawn_read('uname', '-p'))
+        self.hardware_platform = u(spawn_read('uname', '-i'))
+        self.operating_system  = spawn_read('uname', '-o')
+
+
